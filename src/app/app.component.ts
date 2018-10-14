187daa184034229af626ab60e1ed03a39697dbc8
Spyros Desyllas <spyros2283@gmail.com>
2018-10-14 17:29:04 +0100

    assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  visible = true;
+  clickLogs = [];
+  increment = 0;
 
+  onToggleParagraph() {
+    this.visible = !this.visible;
+    this.increment++;
+    this.clickLogs.push(this.increment);
+  }
+
+  getColor() {
+    return this.increment >= 5 ? "blue" : "transparent";
+  }
 }
