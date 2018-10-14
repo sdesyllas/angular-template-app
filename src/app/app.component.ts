691223ce3f93b1609a66150f1a8c13a1c15e7ace
Spyros Desyllas <spyros2283@gmail.com>
2018-10-14 12:04:04 +0100

    assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+  userNotEmpty = false;
 
+  onUpdateUserName(event: Event) {
+    if (this.username != '') {
+      this.userNotEmpty = true;
+    }
+    else {
+      this.userNotEmpty = false;
+    }
+  }
+
+  onClearUserName() {
+    this.username = '';
+    this.userNotEmpty = false;
+  }
 }
